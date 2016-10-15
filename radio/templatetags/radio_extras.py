5f4c84172220b9ac140683e97605927dfd7ad6e3
Dylan Reinhold <dreinhold@gmail.com>
2016-10-14 23:22:41 -0700

    Move the amazon ad settings out of templates
      Moved them into settings

--- radio/templatetags/radio_extras.py
+++ radio/templatetags/radio_extras.py
@@ -1,3 +1,5 @@
+import random
+
 from django import template
 from django.conf import settings
 
@@ -12,3 +14,13 @@ def settings_anonymous_time():
 @register.simple_tag()
 def settings_amazon_adds():
     return getattr(settings, 'AMAZON_ADDS', False)
+
+# All Amazon Settings
+@register.simple_tag()
+def settings_amazon_ad(value):
+    if value.startswith("AMAZON_AD"): # Only expose amazon settings
+        if value == 'AMAZON_AD_FALL_BACK_SEARCH': # Pick from the list
+            return random.choice(getattr(settings, value, False))
+        return getattr(settings, value, False)
+    return None
+
