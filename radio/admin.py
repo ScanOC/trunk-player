cb9f96daadaa4847f5229c4b7e5cbb03e2aaa670
Dylan Reinhold <dylan@gasdasoftware.com>
2017-02-25 10:22:29 -0800

    Add system to unit and transmission lists in admin

--- radio/admin.py
+++ radio/admin.py
@@ -8,11 +8,11 @@ from .models import *
 
 class TalkGroupAdmin(admin.ModelAdmin):
     search_fields = ['alpha_tag', 'description', 'dec_id']
-    list_display = ('alpha_tag', 'description', 'dec_id')
+    list_display = ('alpha_tag', 'description', 'dec_id', 'system')
 
 class UnitAdmin(admin.ModelAdmin): 
     search_fields = ['description', 'dec_id' ]
-    list_display = ('description', 'dec_id' )
+    list_display = ('description', 'dec_id', 'system' )
 
 class TranmissionUnitInline(admin.TabularInline):
     model = TranmissionUnit
