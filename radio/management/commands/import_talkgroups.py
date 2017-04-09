6bd7fa3e54ac5c92095d124cbe9b41d0fc4d0955
Dylan Reinhold <dreinhold@gmail.com>
2017-04-09 09:35:28 -0700

    Import TalkGroups now will update existing rows

--- radio/management/commands/import_talkgroups.py
+++ radio/management/commands/import_talkgroups.py
@@ -13,21 +13,27 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('file')
+        parser.add_argument(
+            '--system',
+            type=int,
+            help='Export talkgroups from only this system',
+            required=True,
+        )
 
     def handle(self, *args, **options):
-        print('Just a test')
-        f_name = options['file']
-        print('You passed in {}'.format(f_name))
-        import_tg_file(f_name)
+        import_tg_file(self, options)
 
 
-def import_tg_file(file_name):
-    ''' Uisng the talkgroup file from trunk-recorder'''
+def import_tg_file(self, options):
+    ''' Using the talkgroup file from trunk-recorder'''
+    file_name = options['file']
+    system = options['system']
+    self.stdout.write("Importing talkgroups for system #{}".format(system))
     with open(file_name) as tg_file:
         tg_info = csv.reader(tg_file, delimiter=',', quotechar='"')
         for row in tg_info:
             try:
-                TalkGroup(dec_id=row[0], alpha_tag=row[3], description=row[4], common_name=row[6]).save()
+                obj, create = TalkGroup.objects.update_or_create(dec_id=row[0], system=system, defaults={'mode': row[2], 'alpha_tag': row[3], 'description': row[4], 'priority': row[5]})
             except (IntegrityError, IndexError):
                 print("Skipping {}".format(row[3]))
 
