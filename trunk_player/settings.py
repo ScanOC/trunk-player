3d791130da34a2f3f429ea0ed543ff98edc3f490
Dylan Reinhold <dreinhold@gmail.com>
2017-06-20 18:19:45 -0700

    Enable redis cache

--- trunk_player/settings.py
+++ trunk_player/settings.py
@@ -170,6 +170,16 @@ CHANNEL_LAYERS = {
     },
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 # Logging
 LOGGING = {
     'version': 1,
