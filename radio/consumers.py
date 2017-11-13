7b369f6b16705bc86a8528a59fe3b538778997cc
Dylan Reinhold <dreinhold@gmail.com>
2017-11-12 20:46:18 -0800

    Include username in WS messages

--- radio/consumers.py
+++ radio/consumers.py
@@ -3,28 +3,29 @@ import json
 import logging
 from channels import Group
 from channels.sessions import channel_session
+from channels.auth import channel_session_user, channel_session_user_from_http
 from .models import ScanList, TalkGroup
 
 log = logging.getLogger(__name__)
 
-@channel_session
+@channel_session_user_from_http
 def ws_connect(message):
     try:
         prefix, tg_type, label = message['path'].strip('/').split('/')
         if prefix != 'ws-calls':
-            log.debug('invalid ws path=%s', message['path'])
+            log.debug('user %s invalid ws path=%s', message.user, message['path'])
             return
     except ValueError:
-        log.debug('invalid ws path=%s', message['path'])
+        log.debug('user %s invalid ws path=%s', message.user, message['path'])
         return
 
-    log.debug('connect %s=%s client=%s:%s', 
-        tg_type, label, message['client'][0], message['client'][1])
+    log.debug('user %s connect %s=%s client=%s:%s', 
+        message.user, tg_type, label, message['client'][0], message['client'][1])
    
     label_list = label.split('+')
     for new_label in label_list: 
         channel_name = 'livecall-{}-{}'.format(tg_type, new_label)
-        log.debug("Connected to channel {}".format(channel_name))
+        log.debug("User {} Connected to channel {}".format(message.user, channel_name))
         message.reply_channel.send({"accept": True})
         Group(channel_name, channel_layer=message.channel_layer).add(message.reply_channel)
 
