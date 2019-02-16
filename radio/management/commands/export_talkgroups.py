8fa5cb6dccfd01df79eee6e9d0b51e72fdeff3a8
kcwebby <bmeinershagen@bmsinetwork.com>
2019-02-16 17:33:19 -0600

    Update export_talkgroups.py

--- radio/management/commands/export_talkgroups.py
+++ radio/management/commands/export_talkgroups.py
@@ -61,5 +61,5 @@ def export_tg_file(self, options):
             if(t.system_id):
                 systemid = t.system_id
 
-tg_file.write("{},{},{},{},{},{},{},{}\n".format(t.dec_id,hex_val,t.mode,alpha,description,service_type,home_site,systemid,t.priority))
+tg_file.write("{},{},{},{},{},{},{},{},{}\n".format(t.dec_id,hex_val,t.mode,alpha,description,service_type,home_site,systemid,t.priority))
 
