cb9e04df72b8b73891665edf45c3b7c31a5f40e4
Max Watermolen <mwateremolen@maxwelldps.com>
2021-09-14 19:57:36 -0500

    typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,9 +11,9 @@ redis-server --daemonize yes
 echo "nginx"
 nginx
 echo "daphne"
-./manage.py runworker livecall-scan-all > runworker1.log 2>&1 &
-./manage.py runworker livecall-scan-all > runworker2.log 2>&1 &
-./manage.py runworker livecall-scan-all > runworker3.log 2>&1 &
-./manage.py runworker livecall-scan-all > runworker4.log 2>&1 &
+./manage.py runworker livecall-scan-default > runworker1.log 2>&1 &
+./manage.py runworker livecall-scan-default > runworker2.log 2>&1 &
+./manage.py runworker livecall-scan-default > runworker3.log 2>&1 &
+./manage.py runworker livecall-scan-default > runworker4.log 2>&1 &
 daphne trunk_player.asgi:channel_layer --port 7055 --bind 127.0.0.1 --access-log /var/log/trunk-player/daphne_main.log --ping-interval 3 --ping-timeout 12 --proxy-headers
 #exec "$@"
