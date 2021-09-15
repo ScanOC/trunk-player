7fa0d89a79d3a526c52ce9353339ff8eca04e89c
Max Watermolen <MAXW@MAXWELLDPS.COM>
2021-09-14 19:12:18 -0500

    fixes runworkers

--- radio/migrations/0063_stabilize.py
+++ radio/migrations/0063_stabilize.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 
+from django.db.migrations.operations.fields import RemoveField
 
 class Migration(migrations.Migration):
 
