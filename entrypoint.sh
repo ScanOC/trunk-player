599abc142920fcf35025d4f4fee21bdd37dc4289
Brett <bctrainers@gmail.com>
2021-12-25 04:06:27 -0600

    Update entrypoint.sh
    Change `--bind` from `127.0.0.1` to `0.0.0.0` for reverse proxy happiness. 
    
    This change may pose a security risk if Trunk-Player is hosted on a web server with no firewall active, as port 7055 becomes exposed without being processed through Apache or Nginx. Ensure a firewall is configured correctly.

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,5 +21,5 @@ echo "daphne"
 ./manage.py runworker livecall-scan-default > runworker2.log 2>&1 &
 ./manage.py runworker livecall-scan-default > runworker3.log 2>&1 &
 ./manage.py runworker livecall-scan-default > runworker4.log 2>&1 &
-daphne trunk_player.asgi:channel_layer --port 7055 --bind 127.0.0.1 --access-log /var/log/trunk-player/daphne_main.log --ping-interval 3 --ping-timeout 12 --proxy-headers
+daphne trunk_player.asgi:channel_layer --port 7055 --bind 0.0.0.0 --access-log /var/log/trunk-player/daphne_main.log --ping-interval 3 --ping-timeout 12 --proxy-headers
 #exec "$@"
