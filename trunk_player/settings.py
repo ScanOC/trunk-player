cb8349dbfb8c51c21b3dd3f61720625802ac1349
penguinzephyr <bayden.allen@gmail.com>
2024-03-20 15:23:22 +1100

    Update settings.py
    Fix typo

--- trunk_player/settings.py
+++ trunk_player/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    ''allauth.account.middleware.AccountMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'trunk_player.urls'
